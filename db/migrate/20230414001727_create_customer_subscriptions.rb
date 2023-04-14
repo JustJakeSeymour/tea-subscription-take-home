8ef41cc77276085e70fb747abd7ba62bf654a5ed
J Seymour <JustJakeSeymour@gmail.com>
2023-04-13 18:29:11 -0600

    create relationships and tests

--- db/migrate/20230414001727_create_customer_subscriptions.rb
+++ db/migrate/20230414001727_create_customer_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateCustomerSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :customer_subscriptions do |t|
-      t.references :customer_id, foreign_key: true
-      t.references :subscription_id, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :subscription, foreign_key: true
 
       t.timestamps
     end
